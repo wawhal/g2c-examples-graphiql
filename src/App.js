aeccb7ad6bdf1dd6bd1d0d3715c27416f007a957
wawhal <rishichandra.wawhal@gmail.com>
2019-03-13 20:42:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import GraphiQL from 'graphiql';
+import '../node_modules/graphiql/graphiql.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const App = () => {
+  const fetcher = (graphQLParams) => {
+    return fetch('https://graphql2chartjs.hasura.app/v1alpha1/graphql', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(graphQLParams),
+    }).then(response => response.json());
   }
+  return (
+    <div>
+      <GraphiQL fetcher={fetcher} />
+    </div>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import GraphiQL from 'graphiql';
+import '../node_modules/graphiql/graphiql.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const fetcher = (graphQLParams) => {
+  return fetch('https://graphql2chartjs.hasura.app/v1alpha1/graphql', {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(graphQLParams),
+  }).then(response => response.json());
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<div style={{height: '100vh'}}> <GraphiQL fetcher={fetcher} /></div>, document.getElementById('root'));
+
